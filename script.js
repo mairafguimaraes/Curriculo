9a319c5dcd6e7a675a4c0bc3fe1c061bb33260b8
Maíra F. Guimarães <137917061+mairafguimaraes@users.noreply.github.com>
2023-07-07 11:57:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function carregaCurriculo(){
     CV01:{
         nome:"Dados Pessoais",
         imagem:"https://cdn-icons-png.flaticon.com/256/4392/4392501.png",
-        descrição:"Paulista - 34 anos. Inquieta, curiosa, leitora, cinéfila. Fui sempre lembrada pela minha memória e organização, então eu vou ser aquela pessoa que lembra qual é o nome da linha 128 do Excel do evento do ano passado. Hoje em transição de carreira, de produção de eventos para a área de tecnologia, mais precisamente em programação front end e desenvolvimento web.",
+        descrição:"Paulista - 34 anos. Inquieta, curiosa, leitora, cinéfila. Sempre fui reconhecida pela minha memória e organização, muito provavelmnente serei aquela pessoa que lembrará qual é o nome da linha 128 do Excel do evento do ano passado. Hoje em transição de carreira, de produção de eventos para a área de tecnologia, mais precisamente em programação front end e desenvolvimento web.",
     },
 
 CV02:{
     nome:"Estudos",
     imagem:"https://cdn-icons-png.flaticon.com/256/4359/4359757.png",
-    descrição:"Graduada Bacharel em Turismo UFSCar, Curso Técnico de Museologia e Produção Cultural ETEC Centro Paula Souza , Curso Livre de Escrita Criativa com Lourenço Mutarelli Sesc Avenida Paulista e Curso Online Front End 01 e 02 PrograMaria.", 
+    descrição:"Graduada Bacharel em Turismo na UFSCar; Curso Técnico de Museologia e Produção Cultural na ETEC Centro Paula Souza; Curso Livre de Escrita Criativa com Lourenço Mutarelli no Sesc Avenida Paulista e Curso Online Front End 01 e 02 na PrograMaria.", 
 },
 
 CV03:{
@@ -22,20 +22,20 @@ CV03:{
 CV04:{
     nome:"Hard Skills",
     imagem:"https://cdn-icons-png.flaticon.com/256/4359/4359739.png",
-    descrição:"Inglês, Excel, Power Point, Word, HTLM,CSS e JavaScript.",
+    descrição:"Inglês, Excel, Power Point, Word, HTLM, CSS e JavaScript.",
 
 },
 
 CV05:{
     nome:"Soft Skills", 
     imagem:"https://cdn-icons-png.flaticon.com/256/4359/4359796.png",
-    descrição:"Comunicação Eficaz, Criatividade, Resiliência, Flexibilidade, Colaboração, Flexibilidade e Inteligência Emocional.",
+    descrição:"Comunicação Eficaz, Criatividade, Resiliência, Colaboração, Flexibilidade e Inteligência Emocional.",
 },
 
 CV06:{
     nome:"Canais de Contato", 
     imagem:"https://cdn-icons-png.flaticon.com/256/4359/4359958.png ",
-    descrição:"Endereço: Rua Martins Fontes, 390, Centro, São Paulo/SP, Whatsapp: 11 97096 6826,  E-mail: mairafguimaraes@gmail.com, Linkedin:https://www.linkedin.com/in/mairafguimaraes/ ,  Github: https://github.com/mairafguimaraes.", 
+    descrição:"Endereço: Rua Martins Fontes, 390, Centro, São Paulo/SP; Whatsapp: 11 97096 6826; E-mail:mairafguimaraes@gmail.com; Linkedin:https://www.linkedin.com/in/mairafguimaraes/ e Github:https://github.com/mairafguimaraes.", 
     },
 
 }; 
